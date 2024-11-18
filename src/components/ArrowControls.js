1d48ec35f790153fc88ddb4b9696773d9238c487
Florian Lemaire <2vsk22@gmail.com>
2024-11-18 11:03:57 +0100

    Animation bug fix

--- src/components/ArrowControls.js
+++ src/components/ArrowControls.js
@@ -2,19 +2,29 @@ import React from 'react';
 import '../css/arrowcontrols.css';
 
 const ArrowControls = ({ onArrowPress, onArrowRelease }) => {
+    const handleTouchStart = (event, direction) => {
+        event.preventDefault(); // Empêche le menu contextuel ou autre comportement par défaut
+        onArrowPress(direction);
+    };
+
+    const handleTouchEnd = (event) => {
+        event.preventDefault(); // Empêche les comportements liés au relâchement
+        onArrowRelease();
+    };
+
     return (
         <div className="arrow-controls">
             <div
                 className="arrow-button left-arrow"
-                onTouchStart={() => onArrowPress("ArrowLeft")}
-                onTouchEnd={onArrowRelease}
+                onTouchStart={(event) => handleTouchStart(event, "ArrowLeft")}
+                onTouchEnd={handleTouchEnd}
                 role="button"
                 aria-label="Flèche gauche"
             ></div>
             <div
                 className="arrow-button right-arrow"
-                onTouchStart={() => onArrowPress("ArrowRight")}
-                onTouchEnd={onArrowRelease}
+                onTouchStart={(event) => handleTouchStart(event, "ArrowRight")}
+                onTouchEnd={handleTouchEnd}
                 role="button"
                 aria-label="Flèche droite"
             ></div>
